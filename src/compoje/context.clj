3ca53ac51c02ac4b32141cab003f82433656ef78
Eugen Stan <eugen.stan@netdava.com>
2023-08-11 14:10:16 +0300

    Allow cli valus to be specified in file

--- src/compoje/context.clj
+++ src/compoje/context.clj
@@ -1,6 +1,40 @@
 (ns compoje.context
   "Functions to manage context"
-  (:require [compoje.utils :as u]))
+  (:require [babashka.fs :as fs]
+            [compoje.utils :as u]
+            [clojure.tools.logging :as log]))
+
+;; TODO: consider whether we should use directory name as default stack name
+#_(defn context-with-stack-name
+  "Build stack name and assoc in context.
+   Stack name is taken in order from:
+
+   - context
+   - cli option"
+  [context template-dir]
+  (let [stack (get-in context [:docker :stack])
+        stack (when-not stack
+                (fs/file-name template-dir))]
+    (when (str/blank? stack)
+      (throw (ex-info "Stack should not be nil" {})))
+    (assoc-in context [:docker :stack] stack)))
+
+(defn final-context
+  "Deep merge contexts, overwriting values."
+  [file-ctx cli-ctx]
+  (log/debug "Merge" file-ctx cli-ctx)
+  (let [template-dir (:template-dir file-ctx)
+        file (str (fs/absolutize (fs/path template-dir "stack.generated.yml")))
+        ctx (u/deep-merge file-ctx cli-ctx {:docker {:compose-file file}})]
+    ;; TODO: some context validations
+    ctx))
+
+(defn stack-name
+  "Stack name from context."
+  [ctx]
+  (let [stack (get-in ctx [:docker :stack])]
+    (assert stack "Stack should not be nil")
+    stack))
 
 (defn load-context!
   "Builds context as a map of values and returns it.
@@ -10,13 +44,10 @@
   [template-dir config secrets]
   (let [cwd (System/getProperty "user.dir")
         values (:values config)
-        values (u/deep-merge {} values secrets)
-        context {:config config
-                 :values values
-                 :secrets secrets
-                 :work-dir cwd
-                 :template-dir template-dir
-                 :output "stack.generated.yml"}]
+        context (u/deep-merge config {:values values
+                                      :secrets secrets
+                                      :work-dir cwd
+                                      :template-dir template-dir})]
     context))
 
 (comment
@@ -24,7 +55,5 @@
   (load-context! "data/stacks/nginx" {} {})
 
   (u/deep-merge {} {:a "b"})
-  (u/deep-merge {} {:a "b"} {:c "d"})
-
-  )
+  (u/deep-merge {} {:a "b"} {:c "d"}))
 
